36cd2def1391e716bfbefb7583cb369771871763
Mauricio Cubillos <mauricio.cubillos@walmart.com>
2025-07-04 10:03:01 -0500

    feat: add new build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   // This is correct for a maocubillos.github.io repository
-  base: "/",
+  base: "./",
   plugins: [react()],
   // This correctly builds to the 'docs' folder for GitHub Pages
   build: {
