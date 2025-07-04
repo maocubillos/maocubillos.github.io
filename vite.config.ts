f18faee777bf41c792bd53941d34356181202022
Mauricio Cubillos <mauricio.cubillos@walmart.com>
2025-07-04 09:46:08 -0500

    feat: add new build

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This is correct for a maocubillos.github.io repository
   base: "/",
-  build: {
-    outDir: path.join(__dirname, "docs/"),
-    rollupOptions: {
-      input: {
-        preview: path.resolve(__dirname, "index.html"),
-      },
-    },
-  },
-  server: {
-    strictPort: true,
-    port: 8001,
-    hmr: {
-      clientPort: 8001,
-    },
-  },
-  root: path.join(__dirname, "/"),
   plugins: [react()],
-  /**
-   * @note Resolve `~` in SCSS modules.
-   * {@see https://github.com/vitejs/vite/issues/5764#issuecomment-982407332}
-   */
-  resolve: {
-    alias: [
-      {
-        find: /^~(.*)$/,
-        replacement: "$1",
-      },
-    ],
+  // This correctly builds to the 'docs' folder for GitHub Pages
+  build: {
+    outDir: "docs",
   },
 });
 
