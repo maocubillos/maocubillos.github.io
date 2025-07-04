4c96eaa77910cf75daddf07cdb911da49a451858
Mauricio Cubillos <mauricio.cubillos@walmart.com>
2025-07-04 09:39:10 -0500

    feat: edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "node:path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/",
+  build: {
+    outDir: path.join(__dirname, "docs/"),
+    rollupOptions: {
+      input: {
+        preview: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  server: {
+    strictPort: true,
+    port: 8001,
+    hmr: {
+      clientPort: 8001,
+    },
+  },
+  root: path.join(__dirname, "/"),
+  plugins: [react()],
+  /**
+   * @note Resolve `~` in SCSS modules.
+   * {@see https://github.com/vitejs/vite/issues/5764#issuecomment-982407332}
+   */
+  resolve: {
+    alias: [
+      {
+        find: /^~(.*)$/,
+        replacement: "$1",
+      },
+    ],
+  },
 });
+
